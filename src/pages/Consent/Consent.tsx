39f4f557a002ea096f0a7c676ca23ec04f1eb513
@amrit_khadka <amrit.khadka@stud.tu-darmstadt.de>
2025-12-19 09:59:47 +0100

    Add CSS import to Consent component for styling

--- src/pages/Consent/Consent.tsx
+++ src/pages/Consent/Consent.tsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSession } from "../../app/session/SessionContext";
-<<<<<<< Updated upstream
-=======
 import "./Consent.css";
->>>>>>> Stashed changes
 
 export default function Consent() {
   const nav = useNavigate();
